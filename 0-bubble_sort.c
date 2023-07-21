cfbe2e855572937262df4afea75d6ff916e19ed6
XimeonLeo <ximeonleo@gmail.com>
2023-07-21 10:55:49 +0100

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts out an array using bubble sort
+ *
+ * @array: the unsorted array
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t first, index, swap, tmp;
+
+	for (first = 0; first < size - 1; first++)
+	{
+		swap = 0;
+		for (index = 0; index < size - 1 - first; index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (swap == 0) /* if there were no swaps */
+		{
+			print_array(array, size);
+			break;
+		}
+	}
+}
